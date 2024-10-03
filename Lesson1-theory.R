692badd3306f000acf19a24dfba731ca9cc43d2b
HKolano <hubertkolano00@gmail.com>
2024-10-03 21:13:29 +0200

    Deleted junk code

--- Lesson1-theory.R
+++ Lesson1-theory.R
@@ -47,13 +47,85 @@ print(lm_fit, digits = 5)
 
 lm_fit$fit |> summary()
 
-colnames(airquality) <- tolower(colnames(airquality))
+lm_fit |> tidy(conf.int = T)
 
-air <-
-  airquality |>
-  as_tibble() |>
-  na.omit() |> 
-  select(-day) |> 
-  mutate(month = factor(month)) 
-air
-ggpairs(air)
+lm_fit |> 
+  tidy() |> 
+  dwplot(vline = geom_vline(xintercept = 0, color = "grey50", linetype = 2), 
+         dot_args = list(size = 2, color = "black"), 
+         whisker_args = list(color = "black")) +
+  theme_bw()
+
+new_points <- expand.grid(initial_volume = seq(5,45,5), 
+                          food_regime = c("Initial", "Low", "High"))
+
+
+# Prognoza średniej wartości
+mean_pred <- predict(object = lm_fit, new_data = new_points)
+
+# Prognoza przedizału ufności
+conf_pred <- predict(object = lm_fit, new_data = new_points, type = "conf_int")
+
+# Łączenie danych
+lm_pred <- 
+  new_points |> 
+  bind_cols(mean_pred) |> 
+  bind_cols(conf_pred)
+
+# WYkres danych
+
+lm_pred |>
+  ggplot(aes(x = food_regime,
+             y = .pred)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = .pred_lower,
+                    ymax = .pred_upper),
+                width = 0.2) +
+  facet_wrap(~ initial_volume) +
+  theme_bw() +
+  labs(y = "urchni size")
+
+prior_dist <- rstanarm::student_t(df = 1)
+
+set.seed(123)
+
+# Ustawiamy metodę estymacji za pomocą parsnip
+
+bayes_mod <-
+  linear_reg() |>
+  set_engine(engine = "stan",
+             prior_intercept = prior_dist,
+             prior = prior_dist)
+
+# Estymacja modelu
+
+bayes_fit <- 
+  bayes_mod |> 
+  fit(width ~ initial_volume * food_regime, data = urchins)
+
+bayes_fit$fit #za mała dokąłdność
+
+bayes_fit |> print(digits = 4)
+bayes_fit |> tidy(conf.int = T)
+
+bayes_pred <- 
+  new_points |> 
+  bind_cols(predict(bayes_fit, new_data = new_points)) |> 
+  bind_cols(predict(bayes_fit, new_data = new_points, type = "conf_int"))
+
+bayes_pred |>
+  ggplot(aes(x = food_regime,
+             y = .pred)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = .pred_lower,
+                    ymax = .pred_upper),
+                width = 0.2) +
+  facet_wrap(~ initial_volume) +
+  theme_bw() +
+  labs(y = "urchni size")
+
+ggplot(urchins,
+       aes(initial_volume, width)) +      # returns a ggplot object 
+  geom_jitter() +                         # same
+  geom_smooth(method = lm, se = FALSE) +  # same                    
+  labs(x = "Volume", y = "Width")         # etc
